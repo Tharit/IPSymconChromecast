5cd3d1e8f3d10fb7d4a639459706c847632b0748
Tharit <mkleinhans@me.com>
2021-04-26 20:44:48 +0200

    updated device module

--- ChromecastDevice/module.php
+++ ChromecastDevice/module.php
@@ -2,10 +2,43 @@
 
 require_once(__DIR__ . '/../libs/protobuf.php');
 
+trait ModuleUtilities {
+    protected function MUSetBuffer($Name, $Daten)
+    {
+        $this->SetBuffer($Name, serialize($Daten));
+    }
+    protected function MUGetBuffer($Name)
+    {
+        return unserialize(this->GetBuffer($Name));
+    }
+    protected function UpdateConnection() {
+        // parent is not available until kernel finished starting
+        if (IPS_GetKernelRunlevel() !== KR_READY) {
+            return;
+        }
+
+        $newParentID = IPS_GetInstance($this->InstanceID)['ConnectionID'];
+        $oldParentID = $this->MUGetBuffer('ConnectionID');
+
+        $this->LogMessage($newParentID . '|' . $oldParentID, KL_NOTIFY);
+        
+        if($newParentID === $oldParentID) return;
+
+        if($oldParentID) {
+            $this->UnregisterMessage($oldParentID, IM_CHANGESTATUS);
+        }
+
+        $this->MUSetBuffer('ConnectionID', $newParentID);
+        $this->RegisterMessage($newParentID, IM_CHANGESTATUS);
+    }
+    protected function GetConnectionID() {
+        return $this->MUGetBuffer('ConnectionID');
+    }
+}
+
 class ChromecastDevice extends IPSModule
 {
-    protected $ParentID = 0;
-    protected $RequestID = 0;
+    use ModuleUtilities;
 
     public function Create()
     {
@@ -28,7 +61,7 @@ class ChromecastDevice extends IPSModule
         $this->RegisterMessage($this->InstanceID, FM_DISCONNECT);
 
         // if this is not the initial creation there might already be a parent
-        $this->UpdateParent();
+        $this->UpdateConnection();
     }
 
     /**
@@ -45,11 +78,12 @@ class ChromecastDevice extends IPSModule
 
         switch ($Message) {
             case IPS_KERNELSTARTED:
-                $this->UpdateParent();
+                $this->UpdateConnection();
                 break;
-            case FM_CONNECT: 
+            case FM_CONNECT:
+
             case FM_DISCONNECT:
-                $this->UpdateParent();
+                $this->UpdateConnection();
                 break;
             case IM_CHANGESTATUS:
                 if ($Data[0] === IS_ACTIVE) {
@@ -70,26 +104,6 @@ class ChromecastDevice extends IPSModule
         $this->SendDebug('Data', $data, 0);
     }
 
-    private function UpdateParent() {
-        // parent is not available until kernel finished starting
-        if (IPS_GetKernelRunlevel() !== KR_READY) {
-            return;
-        }
-
-        $newParentID = IPS_GetInstance($this->InstanceID)['ConnectionID'];
-        
-        $this->LogMessage($newParentID . '|' . $this->ParentID, KL_NOTIFY);
-        
-        if($newParentID == $this->ParentID) return;
-
-        if($this->ParentID) {
-            $this->UnregisterMessage($this->ParentID, IM_CHANGESTATUS);
-        }
-
-        $this->ParentID = $newParentID;
-        $this->RegisterMessage($this->ParentID, IM_CHANGESTATUS);
-    }
-
     private function connect() {
         $c = new CastMessage();
 		$c->source_id = "sender-0";
@@ -97,7 +111,7 @@ class ChromecastDevice extends IPSModule
 		$c->urnnamespace = "urn:x-cast:com.google.cast.tp.connection";
 		$c->payloadtype = 0;
 		$c->payloadutf8 = '{"type":"CONNECT"}';
-        CSCK_SendText($this->ParentID, $c->encode());
+        CSCK_SendText($this->GetConnectionID(), $c->encode());
         //$this->SendDataToParent(json_encode(['DataID' => '{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}', 'Buffer' => utf8_encode($c->encode())]));
     }
 
@@ -109,7 +123,7 @@ class ChromecastDevice extends IPSModule
 		$c->payloadtype = 0;
 		$c->payloadutf8 = '{"type":"GET_STATUS", "requestId":' . ($this->RequestID++) . '}';
 
-        CSCK_SendText($this->ParentID, $c->encode());
+        CSCK_SendText($this->GetConnectionID(), $c->encode());
         //$this->SendDataToParent(json_encode(['DataID' => '{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}', 'Buffer' => utf8_encode($c->encode())]));
     }
 }
