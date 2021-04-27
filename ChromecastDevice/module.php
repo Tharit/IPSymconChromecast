cfa1d5be5b3e078e3f709e85920b5622383e5f0d
Tharit <mkleinhans@me.com>
2021-04-27 08:46:21 +0200

    updated device module

--- ChromecastDevice/module.php
+++ ChromecastDevice/module.php
@@ -1,48 +1,7 @@
 <?php
 
 require_once(__DIR__ . '/../libs/protobuf.php');
-
-trait ModuleUtilities {
-    protected function MUSetBuffer($Name, $Daten)
-    {
-        $this->SetBuffer($Name, serialize($Daten));
-    }
-
-    protected function MUGetBuffer($Name)
-    {
-        return unserialize($this->GetBuffer($Name));
-    }
-
-    protected function UpdateConnection() {
-        // parent is not available until kernel finished starting
-        if (IPS_GetKernelRunlevel() !== KR_READY) {
-            return false;
-        }
-
-        $newParentID = IPS_GetInstance($this->InstanceID)['ConnectionID'];
-        $oldParentID = $this->MUGetBuffer('ConnectionID');
-
-        $this->LogMessage($newParentID . '|' . $oldParentID, KL_NOTIFY);
-        
-        if($newParentID === $oldParentID) return false;
-
-        if($oldParentID) {
-            $this->UnregisterMessage($oldParentID, IM_CHANGESTATUS);
-        }
-
-        $this->MUSetBuffer('ConnectionID', $newParentID);
-        
-        if($newParentID) {
-            $this->RegisterMessage($newParentID, IM_CHANGESTATUS);
-        }
-    
-        return $newParentID > 0;
-    }
-
-    protected function GetConnectionID() {
-        return $this->MUGetBuffer('ConnectionID');
-    }
-}
+require_once(__DIR__ . '/../libs/ModuleUtilities.php');
 
 class ChromecastDevice extends IPSModule
 {
@@ -58,12 +17,18 @@ class ChromecastDevice extends IPSModule
 
         $this->RequireParent('{3CFF0FD9-E306-41DB-9B5A-9D06D38576C3}'); // IO Client Socket
 
+        // properties
         $this->RegisterPropertyString('uuid', '');
         $this->RegisterPropertyString('name', '');
         $this->RegisterPropertyString('type', '');
         $this->RegisterPropertyString('ip', '');
         $this->RegisterPropertyString('port', '');
 
+        // variables
+        $this->RegisterVariableString("ActiveApplication", "Active Application");
+        $this->RegisterVariableFloat("Volume", "Volume", "~Intensity");
+
+        // messages
         $this->RegisterMessage(0, IPS_KERNELSTARTED);
         $this->RegisterMessage($this->InstanceID, FM_CONNECT);
         $this->RegisterMessage($this->InstanceID, FM_DISCONNECT);
@@ -121,15 +86,64 @@ class ChromecastDevice extends IPSModule
 
         if($c->payload_type === 0) {
             $data = json_decode($c->payload_utf8);
-            $this->SendDebug('JSON Data', print_r($data, true), 0);
+            if($data['type'] === 'RECEIVER_STATUS') {
+                if(isset($data['volume'])) {
+                    $this->SetValue("Volume", $data['volume']['level']);
+                }
+                if(isset($data['applications']) && count($data['applications']) === 1) {
+                    $this->SetValue("ActiveApplication", $data['applications'][0]['displayName']);
+                    $this->MUSetBuffer('TransportId', $data['applications'][0]['transportId']);
+                    $this->MUSetBuffer('SessionId', $data['applications'][0]['sessionId']);
+                } else {
+                    $this->SetValue("ActiveApplication", "");
+                    $this->MUSetBuffer('TransportId', '');
+                    $this->MUSetBuffer('SessionId', '');
+                }
+            } else if($data['type'] === 'PING') {
+                $this->Pong();
+            }
+            $this->SendDebug('JSON Data', $c->namespace . '|' . print_r($data, true), 0);
         } else {
             $this->SendDebug('Binary Data', print_r($c->payload_binary, true), 0);
         }
+    }
+    
+    public function RequestAction($ident, $value)
+    {
+        // change volume
+    }
+
+    //------------------------------------------------------------------------------------
+    // external methods
+    //------------------------------------------------------------------------------------
+    public function Stop() {
+        $sessionId = $this->MUGetBuffer('sessionId');
+        if(!$sessionId) return false;
+    
+        $c = new CastMessage();
+		$c->source_id = "sender-0";
+		$c->destination_id = "receiver-0";
+		$c->namespace = "urn:x-cast:com.google.cast.receiver";
+		$c->payload_type = 0;
+		$c->payload_utf8 = '{"type":"STOP","requestId":' . ($this->GetRequestID()) . ',"sessionId":'.$sessionId.'}';
+        CSCK_SendText($this->GetConnectionID(), $c->encode());
+
+        return true;
     }
 
     //------------------------------------------------------------------------------------
     // module internals
     //------------------------------------------------------------------------------------
+    private function Pong() {
+        $c = new CastMessage();
+		$c->source_id = "sender-0";
+		$c->destination_id = "receiver-0";
+		$c->namespace = "urn:x-cast:com.google.cast.tp.heartbeat";
+		$c->payload_type = 0;
+		$c->payload_utf8 = '{"type":"PONG"}';
+        CSCK_SendText($this->GetConnectionID(), $c->encode());
+    }
+
     private function Connect() {
         $c = new CastMessage();
 		$c->source_id = "sender-0";
